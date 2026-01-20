6b9a14115ca22d685f444ef3b0462f44909de180
jirbthagoras <hansjabriel@gmail.com>
2026-01-20 23:07:22 +0700

    anjeeeng

--- hidup.go
+++ hidup.go
@@ -1,9 +1,10 @@
 package go_module_hjkw
 
+// Ini private func ygy
 func sayHidupJokowi() string {
 	return "Hidup Jokowi"
 }
 
-func sayPriaOsloItuLagi() string {
+func SayPriaOsloItuLagi() string {
 	return "Pria Oslo itu Lagi"
 }
